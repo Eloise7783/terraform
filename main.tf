2224e5b7e3923e97502b29f40961e568a63ac700
Eloise Wood <eloise.wood@email.com>
2024-04-10 11:06:53 +0000

    lab 3 added

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ provider "aws" {
     region = "eu-west-2"
 }
 
-provider "aws" {
-    alias = "US"
-    region = "us-west-1"
-    # in resource block, provider = "aws.US"
-}
+# provider "aws" {
+#     alias = "US"
+#     region = "us-west-1"
+#     # in resource block, provider = "aws.US"
+# }
 
 resource "aws_instance" "demo1" {
     ami = "ami-09a2a0f7d2db8baca"
@@ -27,17 +27,17 @@ resource "aws_instance" "demo1" {
 # terraform destroy -target=[resource_type.resource_name.resource_attribute] - not recommended
 
 #commenting out or deleting the referential code will destroy the resources
-locals {
-  project_name = "Eloise"
-}
-resource "aws_instance" "demo2" {
-    ami = "ami-0fb391cce7a602d1f"
-    instance_type = "t2.nano"
-    key_name = "mod5"
-    tags = {
-        Name = "Made by Terraform! -${local.project_name}"
-    }
-}
+# locals {
+#   project_name = "Eloise"
+# }
+# resource "aws_instance" "demo2" {
+#     ami = "ami-0fb391cce7a602d1f"
+#     instance_type = "t2.nano"
+#     key_name = "mod5"
+#     tags = {
+#         Name = "Made by Terraform! -${local.project_name}"
+#     }
+#}
 resource "aws_s3_bucket" "s3-bucket" {
     bucket = var.s3_bucket_name
   
---
+++ output.tf
@@ -0,0 +1,6 @@
+output "vm_public_ip" {
+value = aws_instance.demo1.public_ip
+}
+output "s3_bucket_domain" {
+value = aws_s3_bucket.s3-bucket.bucket_domain_name
+}
